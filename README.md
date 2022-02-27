9a5e89561adbff4077ae0803944606f84302fe6f
Lemuel Nabong <lemuel.nabong@webbfontaine.com>
2022-02-28 00:09:43 +0800

    Add endpoint that reads env var

--- README.md
+++ README.md
@@ -5,8 +5,14 @@
 
 produces: `./build/libs/sb1.jar`
 
+# Spring Boot Run
+`./gradlew bootRun`
+
 # Docker Build
 `docker build -t legnoban/sample-sb-app1 .`
+
+# Docker Push
+`docker push legnoban/sample-sb-app1`
 
 # Docker Run
 `docker run -p 8080:8080 legnoban/sample-sb-app1`
--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+	@GetMapping("/greet")
+	public String greet() {
+		String visitor = System.getenv().getOrDefault("VISITOR", "Anonymous");
+		return "Hello " + visitor;
+	}
+
 	@GetMapping("/")
 	public String index() {
 		return "Welcome to App 1";
